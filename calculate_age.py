d8261d177192b9cf68f744e25979a98105a043f8
Rajan Karki <rajankarkee8@gmail.com>
2023-05-12 10:55:39 +0900

    fizzbuzz

--- calculate_age.py
+++ calculate_age.py
@@ -4,7 +4,7 @@ def Date_Calculate(y ,m, d):
     today = datetime.datetime.now().date()
     d_o_b = datetime.date(y, m, d)  
     
-    age = int((today-d_o_b).days/365)
+    age = int((today - d_o_b).days/365)
     print(age, f'years')
     
 Date_Calculate(2001, 3, 12)
